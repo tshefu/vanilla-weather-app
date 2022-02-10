76da588414bd91394c665bd53eb5501a983a2f99
tshefu <schneider.viktoria95@gmail.com>
2022-02-10 12:21:47 +0100

    removed console.log in js

--- src/app.js
+++ src/app.js
@@ -67,7 +67,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "ab11dfc00b0fc859930d2f7439af6809";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
